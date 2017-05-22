d06d8569280763d47b58618ad26e4f158c3f38bd
Noré <a.sarrazin@gmail.com>
2017-05-22 16:45:20 +0200

    Campagnes en cours
    Campagnes en cours

--- application/controllers/Campagnes.php
+++ application/controllers/Campagnes.php
@@ -40,31 +40,39 @@ class Campagnes extends CI_Controller {
 		if ($_SESSION["is_connect"] == TRUE){
 
 			require(APPPATH.'libraries/Mailin.php');
-					$mailin = new Mailin("https://api.sendinblue.com/v2.0",API_key);
+					$mailin = new Mailin("https://api.sendinblue.com/v2.0",'Q8ySqdN5p4BOG6CH');
 
+					var_dump ($mailin);
 
 			$data = array(
-				"category"			=>API_category,
-				"from_name"			=>API_from_name,
-				"from_email"		=>API_from_email,
-				"name"				=>$this->input->post('name'),
-				"html_content"		=>"<html><head></head></html>",
-				"html_url"			=>"",
-				"listid"			=> "",
-				"scheduled_date"	=>"",
-				"subject"			=>$this->input->post('subject'),
-				"reply_to"			=>API_reply_to,
-				"to_field"			=>"[PRENOM] [NOM]",
-				'exclude_list'		=> array(),
-				"attachment_url"	=>"",
-				"inline_image"		=>0,
-				"mirror_active"		=>1,
-				"send_now"			=>0
+				"category"=>"",
+        "from_name"=>"",
+        "from_email"=>"pierre.atman@gmail.com",
+        "name"=>"My Campaign 1",
+        "bat"=>"pierre.atman@gmail.com",
+        "html_content"=>"<html><head></head></html>",
+        "html_url"=>"",
+        "listid"=> array(),
+        "scheduled_date"=>"",
+        "subject"=>"My subject",
+        "reply_to"=>"pierre.atman@gmail.com",
+        "to_field"=>"[PRENOM] [NOM]",
+        'exclude_list'=> array(),
+        "attachment_url"=>"",
+        "inline_image"=>0,
+        "mirror_active"=>0,
+        "send_now"=>0,
+        "utm_campaign"=>"My UTM Value1"
 			);
 
-				$mailin->create_campaign($data);
 
-				redirect (base_url()."campagnes.html");
+			var_dump ($data);
+
+				$result = $mailin->create_campaign($data);
+
+				var_dump ($result);
+
+				//redirect (base_url()."campagnes.html");
 
 
 			} else {
