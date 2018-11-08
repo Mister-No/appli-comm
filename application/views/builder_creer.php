5b3f4e86f571e823cb3fbdcd8a975bd49473a8a6
Noré <a.sarrazin@gmail.com>
2018-11-08 23:46:49 +0100

    builder block
    builder block

--- application/views/builder_creer.php
+++ application/views/builder_creer.php
@@ -58,6 +58,7 @@
 
 		var id_block;
 		var blockPlace;
+		var newBlockPlace;
 
 		$('.newsBuilderBlock').hover(function(){
 
@@ -85,6 +86,18 @@
 			$(this).before(block_before);
 			$(this).after(block_after);
 
+			// PLACE DU BLOCK
+
+			addBlockPlace = $(this).attr('id');
+
+			if (addBlockPlace == 'after') {
+				newBlockPlace = Number(blockPlace)+1;
+			} else if (addBlockPlace == 'before') {
+				newBlockPlace = Number(blockPlace);
+			} else {
+
+			}
+
 			// BLOCK OPTIONS
 
 			if (blockPlace > 1) {
@@ -110,7 +123,6 @@
 											'</button>'+
 											'<button type="button" class="editBlock">'+
 												'<i class="editIcon fa fa-edit"></i>'+
-											'<button type="button"'+
 											downblock+
 										'</div>';
 
@@ -119,7 +131,6 @@
 			chooseblock();
 			upMoveBlock();
 			downMoveBlock();
-			//editblock();
 			deleteBlock();
 
 		});
@@ -133,72 +144,76 @@
 
 		$('.newsBuilderAddBlock').click( function() {
 
-			chooseBlock = '	<div class="page-container blockSelect fullHeight">'+
-												'<div class="main-content">'+
-													'<div class="row">'+
-														'<div class="closeBlockSelect col-lg-12">'+
-															'<i class="closeIcon pg-close"></i>'+
+			$('.blockSelect').hide();
+
+			chooseBlock = '<div class="page-container blockSelect fullHeight">'+
+											'<div class="main-content">'+
+												'<div class="row">'+
+													'<div class="closeBlockSelect col-lg-12">'+
+														'<i class="closeIcon pg-close"></i>'+
+													'</div>'+
+													'<div class="col-lg-8 newsBlocks clearFloat center-block">'+
+														'<div class="col-xs-2 newBlock imgBlock center-block" data-blocktype="5">'+
+															'<i class="fa fa-image"></i>'+
+															'<p>Image</p>'+
 														'</div>'+
-														'<div class="col-lg-8 newsBlocks clearFloat center-block">'+
-															'<div class="col-xs-2 newBlock imgBlock center-block">'+
-																'<i class="fa fa-image"></i>'+
-																'<p>Image</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock imgTextBlock">'+
-																'<i class="fa fa-image"></i>'+
-																'<i class="pg-grid"></i>'+
-																'<p>Image + Texte</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock titleBlock">'+
-																'<i class="fa fa-font"></i>'+
-																'<p>Titre</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock textBlock">'+
-																'<i class="fa fa-font"></i>'+
-																'<p>Texte</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock spacerBlock">'+
-																'<img class="spacerButton" src="/assets/img/spacer_icon.png" alt="">'+
-																'<p>Marge</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock buttonBlock">'+
-																'<img class="linkButton" src="/assets/img/link_button_icon.png" alt="">'+
-																'<p>Bouton lien</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock spacerBlock">'+
-																'<img class="spacerButton" src="/assets/img/spacer_icon.png" alt="">'+
-																'<p>Marge</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock buttonBlock">'+
-																'<img class="linkButton" src="/assets/img/link_button_icon.png" alt="">'+
-																'<p>Bouton lien</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock spacerBlock">'+
-																'<img class="spacerButton" src="/assets/img/spacer_icon.png" alt="">'+
-																'<p>Marge</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock buttonBlock">'+
-																'<img class="linkButton" src="/assets/img/link_button_icon.png" alt="">'+
-																'<p>Bouton lien</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock spacerBlock">'+
-																'<img class="spacerButton" src="/assets/img/spacer_icon.png" alt="">'+
-																'<p>Marge</p>'+
-															'</div>'+
-															'<div class="col-xs-2 newBlock buttonBlock">'+
-																'<img class="linkButton" src="/assets/img/link_button_icon.png" alt="">'+
-																'<p>Bouton lien</p>'+
-															'</div>'+
+														'<div class="col-xs-2 newBlock imgTextBlock">'+
+															'<i class="fa fa-image"></i>'+
+															'<i class="pg-grid"></i>'+
+															'<p>Image + Texte</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock titleBlock">'+
+															'<i class="fa fa-font"></i>'+
+															'<p>Titre</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock textBlock" data-blocktype="2">'+
+															'<i class="fa fa-font"></i>'+
+															'<p>Texte</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock spacerBlock">'+
+															'<img class="spacerButton" src="/assets/img/spacer_icon.png" alt="">'+
+															'<p>Marge</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock buttonBlock">'+
+															'<img class="linkButton" src="/assets/img/link_button_icon.png" alt="">'+
+															'<p>Bouton lien</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock spacerBlock">'+
+															'<img class="spacerButton" src="/assets/img/spacer_icon.png" alt="">'+
+															'<p>Marge</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock buttonBlock">'+
+															'<img class="linkButton" src="/assets/img/link_button_icon.png" alt="">'+
+															'<p>Bouton lien</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock spacerBlock">'+
+															'<img class="spacerButton" src="/assets/img/spacer_icon.png" alt="">'+
+															'<p>Marge</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock buttonBlock">'+
+															'<img class="linkButton" src="/assets/img/link_button_icon.png" alt="">'+
+															'<p>Bouton lien</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock spacerBlock">'+
+															'<img class="spacerButton" src="/assets/img/spacer_icon.png" alt="">'+
+															'<p>Marge</p>'+
+														'</div>'+
+														'<div class="col-xs-2 newBlock buttonBlock">'+
+															'<img class="linkButton" src="/assets/img/link_button_icon.png" alt="">'+
+															'<p>Bouton lien</p>'+
 														'</div>'+
 													'</div>'+
 												'</div>'+
-											'</div>';
+											'</div>'+
+										'</div>';
 
 			$('.pace-done').append(chooseBlock);
 
 			$('.closeIcon').click( function() {
 				$('.blockSelect').remove();
 			});
+
+			addblock();
 
 		});
 
@@ -207,8 +222,9 @@
 	function addblock() {
 
 		$('.newBlock').click( function() {
-
-			addBlockPlace = $(this).attr('id');
+console.log(newBlockPlace);
+			$('.blockSelect').hide();
+			/**addBlockPlace = $(this).attr('id');
 
 			if (addBlockPlace == 'after') {
 				newBlockPlace = Number(blockPlace)+1;
@@ -216,109 +232,67 @@
 				newBlockPlace = Number(blockPlace);
 			} else {
 
-			}
+			}**/
 
-			chooseBlock = '	<div class="page-container blockSelect fullHeight">'+
-												'<div class="main-content">'+
-													'<div class="row">'+
-														'<div class="closeBlockSelect col-lg-12">'+
-															'<i class="closeIcon pg-close"></i>'+
-														'</div>'+
-														'<form class="col-lg-8 choosenBlockContainer clearFloat center-block" action="<?=base_url();?>builder/update/<?=$id_newsletter?>.html" method="post" enctype="multipart/form-data">'+
-															'<div class="col-xs-10 center-block choosenBlock clearFloat">'+
-															'</div>'+
-															'<input type="hidden" name="ordre" value="'+newBlockPlace+'">'+
-															'<div class="col-xs-10 choosenBlockFooter panel-footer center-block text-right">'+
-																'<button id="return" type="button" class="btn btn-complete">RETOUR</button>'+
-																'<button type="submit" class="btn btn-success">AJOUTER</button>'+
-															'</div>'+
-														'</form>'+
-													'</div>'+
+			addBlock = '<div class="page-container blockSelect fullHeight">'+
+										'<div class="main-content">'+
+											'<div class="row">'+
+												'<div class="closeBlockSelect col-lg-12">'+
+													'<i class="closeIcon pg-close"></i>'+
 												'</div>'+
-											'</div>';
+												'<form class="col-lg-8 choosenBlockContainer clearFloat center-block" action="<?=base_url();?>builder/update/<?=$id_newsletter?>.html" method="post" enctype="multipart/form-data">'+
+													'<div class="col-xs-10 center-block choosenBlock clearFloat">'+
+													'</div>'+
+													'<input type="hidden" name="ordre" value="'+newBlockPlace+'">'+
+													'<div class="col-xs-10 choosenBlockFooter panel-footer center-block text-right">'+
+														'<button id="return" type="button" class="btn btn-complete">RETOUR</button>'+
+														'<button type="submit" class="btn btn-success">AJOUTER</button>'+
+													'</div>'+
+												'</form>'+
+											'</div>'+
+										'</div>'+
+									'</div>';
 
-			$('.pace-done').append(chooseBlock);
+			$('.pace-done').append(addBlock);
 
 			$('#return').click( function() {
-				$('.choosenBlockContainer').css('display', 'none');
-				$('.newsBlocks').show();
+				//$('.choosenBlockContainer').css('display', 'none');
+				chooseblock();
 			});
 
 			$('.closeIcon').click( function() {
 				$('.blockSelect').remove();
 			});
 
-			$('.textBlock').click( function() {
+			blocktype = $(this).data('blocktype');
 
-				$('.newsBlocks').hide();
-				$('.choosenBlockContainer').css('display', 'flex');
-				$('.choosenBlockContent').remove();
+			if (blocktype == 1) {
+				// BLOCK HEADER
+			} else if (blocktype == 2) {
 
+				// BLOCK TEXTE
 				block = '<div class="summernote-wrapper"><textarea id="summernote" class="builderTextarea choosenBlockContent" name="text">Votre texte</textarea><input type="hidden" name="id_block" value="2"></div>';
 
 				$('.choosenBlock').css('min-height', '200px');
 				$('.choosenBlock').append(block);
 				$('#summernote').summernote({
-				  toolbar: [
-				    // [groupName, [list of button]]
-				    ['style', ['bold', 'italic', 'underline', 'clear']],
-				    ['font', ['strikethrough', 'superscript', 'subscript']],
-				    ['fontsize', ['fontsize']],
-				    ['color', ['color']],
-				  ],
+					toolbar: [
+						// [groupName, [list of button]]
+						['style', ['bold', 'italic', 'underline', 'clear']],
+						['font', ['strikethrough', 'superscript', 'subscript']],
+						['fontsize', ['fontsize']],
+						['color', ['color']],
+					],
 					height: 140,
 				});
-			});
 
-			$('.titleBlock').click( function() {
+			} else if (blocktype == 3) {
+				// BLOCK FOOTER
+			} else if (blocktype == 4) {
+				// BLOCK DESABONNEMENT
+			} else if (blocktype == 5) {
 
-				$('.newsBlocks').hide();
-				$('.choosenBlockContainer').css('display', 'flex');
-				$('.choosenBlockContent').remove();
-
-				block = '<input type="text" class="builderInput choosenBlockContent" name="text" placeholder="Votre titre">'+
-				'<input type="hidden" name="id_block" value="2">';
-
-				$('.choosenBlock').css('min-height', '40px');
-				$('.choosenBlock').append(block);
-
-			});
-
-			$('.buttonBlock').click( function() {
-
-				$('.newsBlocks').hide();
-				$('.choosenBlockContainer').css('display', 'flex');
-				$('.choosenBlockContent').remove();
-
-				block = '<input type="text" class="builderInput choosenBlockContent" name="text" placeholder="Titre du bouton">'+
-				'<input type="text" class="builderInput choosenBlockContent" name="text1" placeholder="Votre lien">'+
-				'<input type="hidden" name="id_block" value="3">';
-
-				$('.choosenBlock').css('min-height', '100px');
-				$('.choosenBlock').append(block);
-
-			});
-
-			$('.spacerBlock').click( function() {
-
-				$('.newsBlocks').hide();
-				$('.choosenBlockContainer').css('display', 'flex');
-				$('.choosenBlockContent').remove();
-
-				block = '<label class="choosenBlockContent">Ajouter un espace entre 2 blocs</label>'+
-				'<input type="hidden" name="id_block" value="4">';
-
-				$('.choosenBlock').css('min-height', '40px');
-				$('.choosenBlock').append(block);
-
-			});
-
-			$('.imgBlock').click( function() {
-
-				$('.newsBlocks').hide();
-				$('.choosenBlockContainer').css('display', 'flex');
-				$('.choosenBlockContent').remove();
-
+				// BLOCK IMAGE
 				block = '<label class="choosenBlockContent">Votre image : </label>'+
 				'<input type="file" class="builderInputFile choosenBlockContent" name="img">'+
 				'<input type="hidden" name="id_block" value="5">';
@@ -326,23 +300,35 @@
 				$('.choosenBlock').css('min-height', '40px');
 				$('.choosenBlock').append(block);
 
-			});
+			} else if (blocktype == 6) {
 
-			$('.imgTextBlock').click( function() {
+				// BLOCK TITRE
+				block = '<input type="text" class="builderInput choosenBlockContent" name="text" placeholder="Votre titre">'+
+				'<input type="hidden" name="id_block" value="2">';
 
-				$('.newsBlocks').hide();
-				$('.choosenBlockContainer').css('display', 'flex');
-				$('.choosenBlockContent').remove();
+				$('.choosenBlock').css('min-height', '40px');
+				$('.choosenBlock').append(block);
 
-				block = '<label class="choosenBlockContent">Votre image : </label>'+
-				'<input type="file" class="builderInputFile choosenBlockContent" name="text">'+
-				'<input class="builderInput choosenBlockContent" name="newsLinkButtonBlock" placeholder="Votre texte">'
-				+'<input type="hidden" name="id_block" value="6">';
+			} else if (blocktype == 7) {
+
+				// BLOCK BOUTTON
+				block = '<input type="text" class="builderInput choosenBlockContent" name="text" placeholder="Titre du bouton">'+
+				'<input type="text" class="builderInput choosenBlockContent" name="text1" placeholder="Votre lien">'+
+				'<input type="hidden" name="id_block" value="3">';
 
 				$('.choosenBlock').css('min-height', '100px');
 				$('.choosenBlock').append(block);
 
-			});
+			} else if (blocktype == 8) {
+
+				// BLOCK ESPACE
+				block = '<label class="choosenBlockContent">Ajouter un espace entre 2 blocs</label>'+
+				'<input type="hidden" name="id_block" value="4">';
+
+				$('.choosenBlock').css('min-height', '40px');
+				$('.choosenBlock').append(block);
+
+			}
 
 		});
 
